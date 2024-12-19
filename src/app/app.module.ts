fe732ca18c26ec64dd7a2f8d6b3bb5beed531154
Guilherme Papa de Souza <64666460+Glmmm@users.noreply.github.com>
2024-12-19 16:57:44 -0300

    Inicio projeto cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CardsModule } from './cards/cards.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    CardsModule,
-    BrowserAnimationsModule,
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

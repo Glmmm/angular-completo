b6e0f6f603e8e223a88522d6e616d60f2dfc6007
Guilherme Papa de Souza <64666460+Glmmm@users.noreply.github.com>
2025-01-09 17:00:50 -0300

    FIX: Criação Componente de Filtros

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,9 +1,36 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
-  imports: [MatButtonModule, MatIconModule],
-  exports: [MatButtonModule, MatIconModule],
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatDividerModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    MatSelectModule,
+  ],
+  exports: [
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatDividerModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    MatSelectModule,
+  ],
 })
 export class MaterialModule {}
---
+++ src/app/models/user/address.model.ts
@@ -0,0 +1,7 @@
+export interface Address {
+  rua: string;
+  numero: number;
+  cidade: string;
+  estado: string;
+  pais: string;
+}

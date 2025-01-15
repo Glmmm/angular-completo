c9f8af2f02208d586d7b181b4711ceff99454a8b
Guilherme Papa de Souza <64666460+Glmmm@users.noreply.github.com>
2025-01-15 13:42:38 -0300

    Acessar Elemento com @ViewChild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,17 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  clicked(input: HTMLInputElement) {
-    console.log(input.value);
-    input.value = 'Atualizado';
+  @ViewChild('input') inputEl!: ElementRef<HTMLInputElement>;
+  @ViewChild('div') divEl!: ElementRef<HTMLDivElement>;
+
+  public changeText() {
+    this.inputEl.nativeElement.value = 'Atualizado';
+  }
+  public focus() {
+    this.inputEl.nativeElement.focus();
+  }
+  public changeDiv() {
+    this.divEl.nativeElement.textContent = 'Banana';
   }
 }
-
-const pai = () => {
-  let pai = 'pai'; 
-  const filha = () => {
-    let filha = 'filha';
-    pai = 'teste';
-    //ASSIM POR DIANTE
-  };
-};

e4b030946063cda79bf5f2a30cfc25cda569c69c
Guilherme Papa de Souza <64666460+Glmmm@users.noreply.github.com>
2025-01-16 15:00:35 -0300

    Acessar elementos com @ViewChildren

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,45 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit, AfterViewInit {
-  @ViewChild('input') inputEl!: ElementRef<HTMLInputElement>;
-  // @ViewChild('input', { static: true } ) inputEl!: ElementRef<HTMLInputElement>; ESTATICO = NÃO POSSUI LOGICA ANTERIOR A SUA RENDERIZAÇÃO, OU ALGO ASSIM
+export class AppComponent implements AfterViewInit {
+  @ViewChildren('btn') buttonsEl!: QueryList<ElementRef<HTMLButtonElement>>;
 
-  constructor() {
-    console.log('constructor');
-  }
-  ngOnInit(): void {
-    console.log('onInit');
-  }
+  buttonsList = ['Botao', 'Botao', 'Botao'];
+
   ngAfterViewInit(): void {
-    console.log('afterViewInit');
-    this.inputEl.nativeElement.focus();
+    console.log(this.buttonsEl);
+    
+    this.buttonsEl.changes.subscribe((a) => {
+      console.log(a);
+    });
+  }
+  public changeColor(e: Event) {
+    const btn = e.target as HTMLButtonElement;
+
+    btn.style.backgroundColor = 'orange';
+    btn.style.color = '#fff';
+  }
+  public resetButtons() {
+    this.buttonsEl.forEach((btnEl) => {
+      console.log(btnEl);
+      btnEl.nativeElement.style.backgroundColor = '';
+      btnEl.nativeElement.style.color = 'black';
+    });
+  }
+  public alterarPrimeiro() {
+    // ALGUMAS FORMAS DE ACESSAR E ALTERAR UM ELEMENTO HTML UTILIZANDO VIEWCHILDREN
+    // const first = this.buttonsEl.find((btnEl) => btnEl.nativeElement.className === 'btn-0');
+    // const first = this.buttonsEl.toArray()[0];
+    const first = this.buttonsEl.get(0);
+    this;
+
+    first!.nativeElement.style.backgroundColor = 'red';
+  }
+  public remove() {
+    this.buttonsList.shift();
   }
 }

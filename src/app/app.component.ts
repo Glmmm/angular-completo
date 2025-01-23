e657ff7885ea454e32c926fe10f56c2e2a9d5732
Guilherme Papa de Souza <64666460+Glmmm@users.noreply.github.com>
2025-01-23 15:50:59 -0300

    Manipulacao de elementos usando ElementRef

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,29 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements AfterViewInit, OnInit {
+  constructor(private readonly elRef: ElementRef) {}
+
+  @ViewChild('div') divEl!: ElementRef<HTMLDivElement>;
+
+  ngOnInit(): void {
+    const divEl = this.elRef.nativeElement.querySelector('#outra-div') as HTMLDivElement;
+
+    divEl.textContent = 'Outra Div';
+    divEl.style.backgroundColor = 'blue';
+    divEl.style.color = 'white';
+
+    divEl.addEventListener('click', () => {
+      console.log('Div clicada');
+    });
+    console.log(this.elRef);
+  }
+  ngAfterViewInit(): void {
+    this.divEl.nativeElement.style.backgroundColor = 'white';
+    this.divEl.nativeElement.textContent = 'Div';
+    this.divEl.nativeElement.classList.add('classe');
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { DirectiveComponent } from './directive/directive.component';
-import { NoDirectiveComponent } from './no-directive/no-directive.component';
-import { HighlightDirective } from './directives/highlight.directive';
-import { DisabledDirective } from './directives/disabled.directive';
-import { StyleDirective } from './directives/style.directive';
-import { ClassDirective } from './directives/class.directive';
-import { ListenerDirective } from './directives/listener.directive';
 
 @NgModule({
-  declarations: [AppComponent, DirectiveComponent, NoDirectiveComponent, HighlightDirective, DisabledDirective, StyleDirective, ClassDirective, ListenerDirective],
+  declarations: [AppComponent],
   imports: [BrowserModule, BrowserAnimationsModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],

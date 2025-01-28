086a05e791cd37097f6903339f8d361ceab91721
Guilherme Papa de Souza <64666460+Glmmm@users.noreply.github.com>
2025-01-28 15:53:31 -0300

    Usando select do <ng-content>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CardComponent } from './card/card.component';
+import { TextoComponent } from './texto/texto.component';
 
 @NgModule({
-  declarations: [AppComponent, CardComponent],
+  declarations: [AppComponent, CardComponent, TextoComponent],
   imports: [BrowserModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],

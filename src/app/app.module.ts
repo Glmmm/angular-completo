069462320936b51e115ba555c5fa40834d1cf02b
Guilherme Papa de Souza <64666460+Glmmm@users.noreply.github.com>
2025-01-28 17:36:44 -0300

    Card teste projecao conteudo <ng-content>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CardComponent } from './card/card.component';
-import { TextoComponent } from './texto/texto.component';
 
 @NgModule({
-  declarations: [AppComponent, CardComponent, TextoComponent],
+  declarations: [AppComponent, CardComponent],
   imports: [BrowserModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],

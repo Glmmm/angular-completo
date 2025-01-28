dffa31610e55d393eac8fcaf78d45b59fbaa6853
Guilherme Papa de Souza <64666460+Glmmm@users.noreply.github.com>
2025-01-28 13:54:44 -0300

    Projeção de conteudo <ng-content>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CardsModule } from './cards/cards.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    CardsModule,
-    BrowserAnimationsModule,
-  ],
+  declarations: [AppComponent, CardComponent],
+  imports: [BrowserModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

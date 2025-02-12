659ff3c4b93d6636fc5d22a2528490f8b04c022b
Guilherme Papa de Souza <64666460+Glmmm@users.noreply.github.com>
2025-02-12 14:18:02 -0300

    Usos ngTemplateOutlet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MeuCompComponent } from './meu-comp/meu-comp.component';
-import { CardComponent } from './card/card.component';
 
 @NgModule({
-  declarations: [AppComponent, MeuCompComponent, CardComponent],
+  declarations: [AppComponent, MeuCompComponent],
   imports: [BrowserModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],

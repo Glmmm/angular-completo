6c44e32b2c8179d3686fdc65e9bb9cd4f90b52bf
Guilherme Papa de Souza <64666460+Glmmm@users.noreply.github.com>
2025-02-12 12:26:24 -0300

    Peculiaridade Projeção de Conteúdo (ng-content)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CardsModule } from './cards/cards.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MeuCompComponent } from './meu-comp/meu-comp.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    CardsModule,
-    BrowserAnimationsModule,
-  ],
+  declarations: [AppComponent, MeuCompComponent, CardComponent],
+  imports: [BrowserModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
